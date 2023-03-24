d20daa79ca749eb3b261b4dd6d0c42a2ef943ce9
taibasultana <tsmtaibasultana@gmail.com>
2023-03-24 15:37:21 +0600

    update

---
+++ multiple_inheritance.py
@@ -0,0 +1,16 @@
+class Father():
+    def Skills (self):
+        print ("I enjoy gardening,programming")
+
+class Mother ():
+    def skills (self):
+        print("I love cooking,art")
+
+class Child (Father,Mother):
+    def skills (self):
+        Father.skills(self)
+        Mother.skills(self)
+        print("sports")
+
+c= Child ()
+c.skills()
